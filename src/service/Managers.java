7d92b9a25526ae071a292812673fbd24a151eff7
anna-sergeeva <55394206+anna-sergeeva@users.noreply.github.com>
2025-03-02 11:25:56 +0000

    Внесены изменения в класс Managers конструктор Managers

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -1,7 +1,8 @@
 package service;
 
 public class Managers {
-    private Managers() {};
+    private Managers() {
+    }
 
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
