d7cea00b81fc17e94f9f3ea333a4a56002c2e08e
anna-sergeeva <55394206+anna-sergeeva@users.noreply.github.com>
2025-03-05 19:18:43 +0000

    Исправление замечаний в ManagersTest

--- test/service/ManagersTest.java
+++ test/service/ManagersTest.java
@@ -6,8 +6,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class ManagersTest {
 
     @Test
-    public void testDefaultManagersNotNull() {
+    public void getDefaultManagersNotNullTest() {
         assertNotNull(Managers.getDefault(), "default manager не должен быть равен null");
+    }
+
+    @Test
+    public void getDefaultHistoryManagersNotNullTest() {
         assertNotNull(Managers.getDefaultHistory(), "default history managers не должен быть равен null");
     }
 }
