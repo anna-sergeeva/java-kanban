4c0d52afa74612fc5bd30e4a08993a7215e9fef7
anna-sergeeva <55394206+anna-sergeeva@users.noreply.github.com>
2025-04-13 19:29:52 +0000

    Исправления в TaskFormatterTest

--- test/service/TaskFormatterTest.java
+++ test/service/TaskFormatterTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import model.*;
 import service.TaskFormatter;
+
 import java.time.Duration;
 import java.time.LocalDateTime;
 
@@ -16,28 +17,28 @@ public class TaskFormatterTest {
 
     @BeforeEach
     public void setUp() {
-        task = new Task(1, "Task", "Test", Duration.ofMinutes(5), LocalDateTime.of(5, 5, 5, 5, 5));
-        epic = new Epic(2, "Epic", "Test");
-        subtask = new Subtask(3, "Subtask", "Test", 2, Duration.ofMinutes(10), LocalDateTime.of(2, 2, 2, 2, 2));
+        task = new Task(1, "Задача", "Тестовая задача", Duration.ofMinutes(5), LocalDateTime.of(5, 5, 5, 5, 5));
+        epic = new Epic(2, "Эпик", "Тестовый эпик");
+        subtask = new Subtask(3, "Подзадача", "Тестовая подзадача", 2, Duration.ofMinutes(10), LocalDateTime.of(2, 2, 2, 2, 2));
     }
 
     @Test
     public void toStringTest() {
-        assertEquals("1,TASK,Task,NEW,Test,PT5M,0005-05-05T05:05", TaskFormatter.toString(task),
-                "toString() некорректно переводит Task в String");
-        assertEquals("2,EPIC,Epic,NEW,Test,PT0S,null", TaskFormatter.toString(epic),
-                "toString() некорректно переводит Epic в String");
-        assertEquals("3,SUBTASK,Subtask,NEW,Test,PT10M,0002-02-02T02:02,2", TaskFormatter.toString(subtask),
-                "toString() некорректно переводит Subtask в String");
+        assertEquals("1,TASK,Задача,NEW,Тестовая задача,PT5M,0005-05-05T05:05", TaskFormatter.toString(task),
+                "функция toString() некорректно переводит Task (задачу) в String (строку)");
+        assertEquals("2,EPIC,Эпик,NEW,Тестовый эпик,PT0S,null", TaskFormatter.toString(epic),
+                "функция toString() некорректно переводит Epic (эпик) в String (строку)");
+        assertEquals("3,SUBTASK,Подзадача,NEW,Тестовая подзадача,PT10M,0002-02-02T02:02,2", TaskFormatter.toString(subtask),
+                "функция toString() некорректно переводит Subtask (подзадачу) в String (строку)");
     }
 
     @Test
     public void fromStringTest() {
         assertEquals(task, TaskFormatter.fromString("1,TASK,Task,NEW,Test,PT10M,0001-01-01T01:01"),
-                "taskFromString() некорректно переводит String в Task");
+                "функция fromString() некорректно переводит String (строку) в Task (задачу)");
         assertEquals(epic, TaskFormatter.fromString("2,EPIC,Epic,NEW,Test,PT0M,0001-01-01T01:01"),
-                "taskFromString() некорректно переводит String в Epic");
+                "функция fromString() некорректно переводит String (строку) в Epic (эпик)");
         assertEquals(subtask, TaskFormatter.fromString("3,SUBTASK,Subtask,NEW,Test,PT10M,0002-02-02T02:02,2"),
-                "taskFromString() некорректно переводит String в Subtask");
+                "функция fromString() некорректно переводит String (строку) в Subtask (подзадачу)");
     }
 }
